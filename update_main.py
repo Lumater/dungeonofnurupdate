8951fa9fc115ebb1a0c80b6cc3c761b36c562e8a
Lumater <lumaterlord@gmail.com>
2017-05-10 10:45:13 -0400

    Edited.

--- update_main.py
+++ update_main.py
@@ -1,3 +1,7 @@
-class Sprite():
-    def __init__(self):
-      pass
+#Add to Player1 and Player2 classes a apic and mpic vars and e.t.c.
+
+def change_mode(obj, mode):
+    if mode == 1:
+        pass
+    if mode == 0:
+        pass
